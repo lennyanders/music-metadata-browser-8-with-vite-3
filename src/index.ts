28bc592464a50e6c0be8672a9169c393393fecc0
LennyAnders <hey@lenny.fyi>
2022-09-05 21:05:09 +0200

    working with music-metadata and polyfills

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-import { parseBlob } from 'music-metadata-browser';
+import { parseBuffer } from 'music-metadata';
+import { Buffer } from 'buffer';
+
+// @ts-ignore
+window.Buffer = Buffer;
 
 const input = document.getElementById('js-load-file-metadata') as HTMLInputElement;
 input?.addEventListener('change', async () => {
   const file = input.files?.[0];
   if (!file) return;
 
-  console.log(await parseBlob(file));
+  console.log(await parseBuffer(Buffer.from(await file.arrayBuffer())));
 });
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  define: {
+    'global.Uint8Array': 'Uint8Array',
+    'process.env.NODE_DEBUG': false,
+  },
+  resolve: {
+    alias: {
+      'node:buffer': 'buffer',
+    },
+  },
+});
